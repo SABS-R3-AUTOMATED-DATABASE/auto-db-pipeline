77367779dce3a4cd3ff74911f36d7eefe5d87439
gemma gordon <gemmagordon2012@gmail.com>
2021-11-24 11:59:13 +0000

    Added ensembl-genbank conversion

--- genbank_mining.py
+++ genbank_mining.py
@@ -2,20 +2,25 @@
 
 # TODO 
 # you get a bunch of information back in the records object that might be useful for assembly
-# you could use ANARCI to parse the sequence just for the VH/VL portion  NOTE how??
+# you could use ANARCI to parse the sequence just for the VH/VL portion  
 # there are other methods besides efetch that might be helpful depending on what you need
+# if error len(seq) not a multiple of 3: 'Explicitly trim the sequence or add trailing N before translation'
 
 from Bio import Entrez
 from Bio.Seq import Seq
 
 # input genbank ID 
-id = input('input genbank id')
 
-# get nucleotide sequence using entrez and convert to amino acid sequence 
-Entrez.email = "gemma.gordon@dtc.ox.ac.uk" #replace with your email address
-handle = Entrez.efetch(db="nucleotide", id=id, retmode="xml") #change id as appropriate or iterate over a file of accession IDs
-records = Entrez.read(handle)
+def genbank_mining(genbank_id):
 
-amino_seq = str(Seq(records[0]["GBSeq_sequence"]).translate()) # NOTE presents error if len(seq) not a multiple of 3 
+    # get nucleotide sequence using entrez and convert to amino acid sequence 
+    Entrez.email = "gemma.gordon@dtc.ox.ac.uk" #replace with your email address
+    handle = Entrez.efetch(db="nucleotide", id=genbank_id, retmode="xml") #change id as appropriate or iterate over a file of accession IDs
+    records = Entrez.read(handle)
+
+    amino_seq = str(Seq(records[0]["GBSeq_sequence"]).translate()) 
+    
+    return amino_seq
 
 
+genbank_mining('AY851612')
--- seq_converter.py
+++ seq_converter.py
@@ -5,10 +5,11 @@ set of functions to convert between different formats of seq, ID so that other i
     # check whether seq is nt or aa 
     # pipe to correct function depending on outcome 
     # accounting for 3-letter AA codes for modified AA (i.e. non-standard 3-letter codes) 
+    # resolve pyentrezid import issue 
 
 from bidict import bidict
 
-def aa_convert_3_1(seq):
+def aa_convert_3_1(aa_3_seq):
 
     '''create birectional dict for amino acid 3-letter abbreviations and single-letter symbols'''
 
@@ -27,9 +28,9 @@ def aa_convert_3_1(seq):
         # determine VH and VL seqs
         # save output in variable
     
-    return # seq in single letter format 
+    return aa_1_seq # seq in single letter format 
 
-def nt_to_aa():
+def nt_to_aa(nt_seq):
 
     ''' convert nucleotide sequences e.g. from genbank to amino acid sequences'''
     
@@ -37,10 +38,23 @@ def nt_to_aa():
     # NOTE ORF problem?
     # again need to identify VH/VL and extract output in variable 
 
-    return 
+    return aa_1_seq
 
-def ensembl_to_genbank():
 
+from PyEntrezId import Conversion 
+# from Conversion import Conversion 
+def ensembl_to_genbank(ensembl_id):
+    
     ''' converts ensembl ID scraped from paper to genbank ID so that sequence (and then AA seq, PDB structure) can be mined'''
-    # https://github.com/lwgray/pyEntrezId or mygene package 
-    return 
+    
+    # from https://github.com/lwgray/pyEntrezId 
+    EnsemblId = ensembl_id
+    # include your email address
+    Id = Conversion('gemma.gordon@dtc.ox.ac.uk')
+    EntrezId = Id.convert_ensembl_to_entrez(EnsemblId)
+    # Returns a string
+    print(EntrezId)
+    
+    return genbank_id
+
+ensembl_to_genbank('ENST00000407559')
