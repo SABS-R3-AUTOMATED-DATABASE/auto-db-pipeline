143fc6548fdca3453197c6b078884d64d892011a
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2021-12-08 13:25:37 +0000

    resolving conflict

---
+++ keywords/tests/test_keywords.py
@@ -0,0 +1,21 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from unittest.mock import patch
+import pytest
+import pandas as pd
+
+from keywords import Keywords
+
+class TestKeywords(unittest.TestCase):
+    '''class to test Keywords'''
+
+    def test_initialize(self):
+
+        with patch.object(pd,'read_csv') as mock_read_csv:
+            CoV = Keywords('./test/path.csv')
+            mock_read_csv.assert_called_with('./test/path.csv')
+
+
+if __name__ == '__main__':
+    unittest.main()
