195376e84033ea965999566258b539a808495434
Emily Jin <emilyjin@fb.com>
2021-12-17 00:32:53 -0500

    finish updating base get_supp_seqs function

--- source/parse_supp/get_supp_seqs.py
+++ source/parse_supp/get_supp_seqs.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import tempfile
 
 
-# TODO: split out smaller function when adding more edge cases
+# TODO: split out into smaller functions when adding more edge cases
 def get_supp_seqs(url):
     # find root domain of url using 3rd occurance of '/'
     ix = -1
@@ -32,7 +32,64 @@ def get_supp_seqs(url):
     if supp_links[0][:4] != 'http':
         supp_links = [root_domain + link for link in supp_links]
 
-    return df_seqs
+    df_seqs = []
+
+    for link in supp_links:
+        seq_dict = {
+            'heavy_aa': [],
+            'light_aa': [],
+            'heavy_nt': [],
+            'light_nt': [],
+        }
+
+        # read data file into dataframe
+        table_data = requests.get(link)
+        with tempfile.TemporaryFile() as tmp:
+            tmp.write(table_data.content)
+            if link[-3] == 'csv':
+                df_file = pd.read_csv(tmp).dropna(axis=1, how='all')
+            else:
+                # TODO: rewrite to account for multilp sheets
+                df_file = pd.read_excel(tmp).dropna(axis=1, how='all')
+
+        # check end of dataframe for columns with long all capital string entries
+        # TODO: find more robust way of getting representative sample
+        df_tail = df_file.tail()
+
+        mask = []
+        for column in df_tail:
+            df_tail[column] = df_tail[column].astype('str')
+
+            # correct for potential utf-8 encoding
+            s = df_tail[column].iloc[0].replace(u'\ufeff', '')
+            if s.isalpha() and s.isupper() and len(s) > 20:
+                mask.append(column)
+
+        # filter for wanted columns with sequences
+        df_file = df_file[mask]
+        df_file = df_file.dropna(axis=0, how='all')
+
+        # if no sequence columns found, return null
+        if len(df_file) == 0:
+            continue
+
+        # if sequences found, classify into heavy/light aa/nt
+        # TODO: find more robust way of parsing column headers
+        for column in df_file:
+            if set(df_file[column].iloc[-1]).issubset({'A', 'G', 'C', 'T'}):
+                if 'l' in column or 'L' in column:
+                    seq_dict['light_nt'] = list(df_file[column])
+                else:
+                    seq_dict['heavy_nt'] = list(df_file[column])
+            else:
+                if 'l' in column or 'L' in column:
+                    seq_dict['light_aa'] = list(df_file[column])
+                else:
+                    seq_dict['heavy_aa'] = list(df_file[column])
+
+        df_seqs.append(pd.DataFrame.from_dict(seq_dict))
+
+    return pd.concat(df_seqs)
 
 
 if __name__ == "__main__":
