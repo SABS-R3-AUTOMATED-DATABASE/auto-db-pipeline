3e03b5744b0b23ebe8a76757353a82de3865fa88
gemma gordon <gemmagordon2012@gmail.com>
2022-01-26 14:24:23 +0000

    PDB_to_seq refactoring

--- downstream_in_use/PDB_to_seq.py
+++ downstream_in_use/PDB_to_seq.py
@@ -3,7 +3,6 @@
 # NOTE CDR identification, need to fill in missing ends of seqs with corresponding germline seq?
 
 from Bio.PDB.PDBList import PDBList
-from Bio import SeqIO
 import subprocess
 import os
 import shutil
@@ -12,8 +11,8 @@ import warnings
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonWarning)
 
-
-
+from verify_antibody_refactored import parse_pdb_file
+from verify_antibody_refactored import create_anarci_input
 
 def pdb_to_seq(verified_antibodies):
 
@@ -28,21 +27,13 @@ def pdb_to_seq(verified_antibodies):
         pdb_file = pdbl.retrieve_pdb_file(pdb_id, file_format='pdb',
                                           overwrite=True, pdir='temp_pdb')
 
-        # get sequence from PDB file using SeqIO
+        # get sequence from PDB file using SeqIO (imported parse_pdb_file function)
         print('Getting sequence...')
-        record_ids = []
-        record_seqs = []
-        for record in SeqIO.parse(pdb_file, 'pdb-atom'):
-            record_ids.append('>' + str(record.id))
-            record_seq = str(record.seq).replace('X', '')
-            record_seqs.append(record_seq)
-            seq_dict = dict(zip(record_ids, record_seqs))
+        seq_dict = parse_pdb_file(pdb_file)
 
         # write dict to FASTA file to input to ANARCI
         print('Creating FASTA file for ANARCI...')
-        with open('anarci_input.fasta', 'a+') as anarci_input:
-            for key, value in seq_dict.items():
-                anarci_input.write('%s\n%s\n' % (key, value))
+        anarci_input = create_anarci_input(seq_dict)
 
         # run ANARCI with FASTA file
         print('Running ANARCI...')
@@ -52,35 +43,9 @@ def pdb_to_seq(verified_antibodies):
         # read anarci output file
         print('Reading ANARCI output...')
         anarci_output = open('anarci_output.txt', 'r')
-        # extract sequence of residues from those annotated with H or L
-        extract_seqs = anarci_output.readlines()
 
-        # set up empty lists for seqs and residue counters
-        VH_seq_list = []
-        VL_seq_list = []
-
-        print('Extracting heavy and light chain sequences...')
-        # limit additions to the sequence to 128 characters (expected length of H seq from ANARCI annotation)
-        for line in extract_seqs:
-            if line.startswith('H') is True:
-                if len(VH_seq_list) < 128:
-                    # 11th character in line (including whitespace) is AA residue
-                    # add character with index [10] to string for sequence (python indexing from 0)
-                    VH_seq_list.append(line[10])
-
-            # same method of extracting seq for VL, except 127 characters total
-            # this also stops seqs that are repeated in FASTA file (e.g. from identical chains) all being added/parsed unnecessarily
-            elif line.startswith('L') is True:
-                if len(VL_seq_list) < 127:
-                    VL_seq_list.append(line[10])
-
-        # convert list of residues to string sequence
-        VH_seq = ''.join([residue for residue in VH_seq_list])
-        VL_seq = ''.join([residue for residue in VL_seq_list])
-
-        print('Printing sequences...')
-        print(pdb_id, 'VH: ', VH_seq)
-        print(pdb_id, 'VL: ', VL_seq)
+        # extract VH and VL sequences
+        extract_VH_VL(pdb_id=pdb_id, anarci_output=anarci_output)
 
         # close temp files/pdb dirs and remove before next iteration
         anarci_input.close()
@@ -91,7 +56,42 @@ def pdb_to_seq(verified_antibodies):
         # obsolete folder exists depending on PDB structure, ignore error if doesn't exist
         shutil.rmtree('obsolete', ignore_errors=True)
 
-    return VH_seq, VL_seq
+    return
+
+
+def extract_VH_VL(pdb_id, anarci_output):
+
+    # extract sequence of residues from those annotated with H or L
+    extract_seqs = anarci_output.readlines()
+
+    # set up empty lists for seqs and residue counters
+    VH_seq_list = []
+    VL_seq_list = []
+
+    print('Extracting heavy and light chain sequences...')
+    # limit additions to the sequence to 128 characters (expected length of H seq from ANARCI annotation)
+    for line in extract_seqs:
+        if line.startswith('H') is True:
+            if len(VH_seq_list) < 128:
+                # 11th character in line (including whitespace) is AA residue
+                # add character with index [10] to string for sequence (python indexing from 0)
+                VH_seq_list.append(line[10])
+
+    # same method of extracting seq for VL, except 127 characters total
+    # this also stops seqs that are repeated in FASTA file (e.g. from identical chains) all being added/parsed unnecessarily
+        elif line.startswith('L') is True:
+            if len(VL_seq_list) < 127:
+                VL_seq_list.append(line[10])
+
+    # convert list of residues to string sequence
+    VH_seq = ''.join([residue for residue in VH_seq_list])
+    VL_seq = ''.join([residue for residue in VL_seq_list])
+
+    print('Printing sequences...')
+    print(pdb_id, 'VH: ', VH_seq)
+    print(pdb_id, 'VL: ', VL_seq)
+
+    return [VH_seq, VL_seq]
 
 
 # test run function
