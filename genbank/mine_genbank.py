0741d4d36d2e79c239695bd0c9d2c584066349b2
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2022-02-06 19:13:13 +0000

    functions added to combine heavy and light chains

--- genbank/mine_genbank.py
+++ genbank/mine_genbank.py
@@ -14,7 +14,14 @@ class GenbankSearch:
     -------
 
     get_number_of_entries(self, keywords)
-    get_entries(self, db='protein')
+    get_entries(self, reduce_searches=False, db='protein')
+    filter_AB_entries(self)
+    classify_vh_vl(self)
+    find_antigen(self)
+    find_fragment_id(self)
+    group_by_author_title(self)
+    pair_vh_vl(self)
+
     __call__(self, db='protein')
     '''
     def __init__(self, all_keywords):
@@ -36,29 +43,40 @@ class GenbankSearch:
         download all entries in the database for the given keywords
         default searches the protein database, possible to change the searched
         database
+        if argument reduce_seraches (int) is passed only the first <int>
+        entries are downloaded
         '''
         if reduce_searches:
-            self.entires_to_retrieve = reduce_searches
+            self.entries_to_retrieve = reduce_searches
         else:
-            self.entires_to_retrieve = self.number_of_entries
+            self.entries_to_retrieve = self.number_of_entries
 
         id_handle = Entrez.esearch(db=db, term=self.search_query,
-                                   retmax=self.entires_to_retrieve)
+                                   retmax=self.entries_to_retrieve)
         record = Entrez.read(id_handle)
 
         # Entrez.efetch handles aproximately 25 searches per second
-        entires_handle = Entrez.efetch(db=db, id=record['IdList'],
+        entries_handle = Entrez.efetch(db=db, id=record['IdList'],
                                        rettype="gb", retmode="xml")
-        self.entries = Entrez.read(entires_handle)
+        self.entries = Entrez.read(entries_handle)
+
+    def filter_AB_entries(self):
+        '''function that checks if entries are antibodies by running their sequences
+        through ANARCI
+        '''
+        pass
 
     def classify_vh_vl(self):
         '''
-        funtion that detects if sequence entries are heavy or light chains
+        funtion that detects if entries are heavy or light chains,
+        searches definitions of genbank entry for keywords linked
+        to heavy or light chains then labels them accordingly
         '''
         for entry in self.entries:
             definition = entry['GBSeq_definition'].lower()
-            hc = ['heavy', 'alpha', 'delta', 'epsilon', 'gamma', 'mu']
-            lc = ['light', 'kappa', 'lambda']
+            hc = ['heavy', 'alpha', 'delta', 'epsilon',
+                  'gamma', 'mu', 'vh', 'vhh']
+            lc = ['light', 'kappa', 'lambda', 'vl']
 
             if any(word in definition for word in hc):
                 entry['chain'] = 'heavy_chain'
@@ -67,16 +85,83 @@ class GenbankSearch:
             else:
                 entry['chain'] = 'unassigned'
 
+    def find_antigen(self):
+        '''
+        function that finds the antigen which an antibody binds to,
+        searches defintions of the genbank entries for certain
+        keywords and labels them accordingly
+        '''
+        for entry in self.entries:
+            definition = entry['GBSeq_definition'].lower()
+            title = entry['GBSeq_references'][0]['GBReference_title'].lower()
+            covid = ['coronavirus', 'sars-cov', 'sars']
+            mers = ['mers-cov', 'mers']
+            cov_2 = ['sars-cov-2', 'covid-19']
+            cov_1 = ['sars-cov-1']
+            spike = ['spike', 'spike protein']
+            rbd = ['receptor binding domain', 'rbd']
+
+            if (any(word in definition for word in cov_2)
+                    or any(word in title for word in cov_2)):
+                entry['antigen'] = 'SARS-CoV-2'
+                if (any(word in definition for word in spike)
+                        or any(word in title for word in spike)):
+                    entry['antigen'] = 'SARS-CoV-2, Spike protein'
+                if (any(word in definition for word in rbd)
+                        or any(word in title for word in rbd)):
+                    entry['antigen'] = 'SARS-CoV-2, Spike protein RBD'
+
+            elif (any(word in definition for word in cov_1)
+                    or any(word in title for word in cov_1)):
+                entry['antigen'] = 'SARS-CoV-1'
+
+            elif (any(word in definition for word in covid)
+                    or any(word in title for word in covid)):
+                entry['antigen'] = 'SARS-CoV'
+
+            elif (any(word in definition for word in mers)
+                    or any(word in title for word in mers)):
+                entry['antigen'] = 'MERS-CoV'
+
+            else:
+                entry['antigen'] = 'not determined'
+
+    def find_fragment_id(self):
+        '''
+        finds the ID of the antibody fragment the entries belongs to
+
+        to find id, entry deinitions are searched for words containing
+        both letters and number and do not belong to one of the exceptions
+        '''
+        for entry in self.entries:
+            definition = entry['GBSeq_definition'].lower()
+            # remove interfering characters from definitions
+            definition = definition.replace(',', '')
+            def_words = definition.split(' ')
+            exceptions = ['sars-cov-2', 'sars-cov-1', 'covid-19', 'sars-cov',
+                          'mers-cov', 'anti-sars-cov', 'anti-sars-cov-2',
+                          'anti-sars-cov-1']
+            for word in def_words:
+                # look for words in the defintions that contain both numbers
+                # and letters
+                if not word.isalpha() and not word.isnumeric():
+                    # check that the word is not in the list of exceptions
+                    if word not in exceptions:
+                        frag_id = word
+                        entry['fragment_id'] = frag_id.upper()
+                        break
+
     def group_by_author_title(self):
         '''
-        function that groups genbank entries that have the same author and
-        title into a list
+        function that groups genbank entries by their publicaiton,
+        entries with the same authors and title are defined to be
+        from the same publication
         '''
         self.grouped_entries = []
-        grouped_entires = []
+        grouped_entries = []
 
         for entry in self.entries:
-            if entry not in grouped_entires:
+            if entry not in grouped_entries:
                 authors = entry['GBSeq_references'][0]['GBReference_authors']
                 title = entry['GBSeq_references'][0]['GBReference_title']
 
@@ -92,25 +177,112 @@ class GenbankSearch:
                             and comparison_title == title):
                         group.append(comparison)
 
-                        grouped_entires.append(comparison)
+                        grouped_entries.append(comparison)
 
                 self.grouped_entries.append(group)
 
+    def pair_vh_vl(self):
+        '''
+        pair corresponding heavy and light chains
+
+        the heavy and light chains are paired with two differnt approches:
+            1. if a group of entries only contains one heavy and light chain
+               they are paired right away
+            2. if a heavy and light chain have the same fragment id they are
+               paired (if there are multiple HC and LC with the same id the
+               first occurence of the HC is paired with the first occurance
+               of the LC)
+
+        Pairings are arranged in a dict with keys "heavy_chain" and
+        "light chain" all pairings are saved as a list in seld.paired_entries
+        Unpaired entries of each group are saved in a list, all lists
+        corresponding to the groups are saved as a list in
+        self.unpaired_entroes
+        '''
+        self.paired_entries = []
+        self.unpaired_entries = []
+        for group in self.grouped_entries:
+
+            heavy_chains = []
+            light_chains = []
+            unassigned = []
+
+            for entry in group:
+                if entry['chain'] == 'heavy_chain':
+                    heavy_chains.append(entry)
+                elif entry['chain'] == 'light_chain':
+                    light_chains.append(entry)
+                else:
+                    unassigned.append(entry)
+
+            # if there is only 1 heavy and one light chain in a publication
+            # they are paired
+            if len(heavy_chains) == 1 and len(light_chains) == 1:
+                dic = {'heavy_chain': heavy_chains[0],
+                       'light_chain': light_chains[0]}
+                self.paired_entries.append(dic)
+                self.unpaired_entries.append(unassigned)
+
+            # if a hc has the same fragment id as a lc they are paired
+            else:
+                unpaired_hc = []
+
+                # loop through all heavy chain entries
+                for hc in heavy_chains:
+                    # get fragment id of hc that have an id
+                    try:
+                        frag_id_hc = hc['fragment_id']
+
+                        # loop through light chains
+                        for lc in light_chains:
+                            # get fragment id of lc that have an id
+                            try:
+                                frag_id_lc = hc['fragment_id']
+
+                                # pair the two chains is ids match
+                                if frag_id_hc == frag_id_lc:
+                                    self.paired_entries.append(
+                                        {'heavy_chain': hc,
+                                         'light_chain': lc}
+                                        )
+                                    # remove the light chains that were paired
+                                    # from list so they can be matched to
+                                    # another hc
+                                    light_chains.remove(lc)
+
+                                    # after the first hc-lc match break the
+                                    # loop and proceed to next hc
+                                    break
+
+                                # if a hc is not paired with any lc add it to
+                                # the list of unpaired hcs
+                                unpaired_hc.append(hc)
+
+                            # ignore lc if it doesn't have an id, will be
+                            # added to unpaired entries later
+                            except KeyError:
+                                pass
+
+                    # if a hc does not have an id add it to unpaired hcs
+                    except KeyError:
+                        unpaired_hc.append(hc)
+
+                # all unpaired hc, lc and sequences of which a chain could
+                # not be determined are added to the unpaired entries
+                unpaired = unpaired_hc + light_chains + unassigned
+                self.unpaired_entries.append(unpaired)
+
     def __call__(self, reduce_searches=False, db='protein'):
         '''
-        runs the functions in order and returns the database entries
-
-        returns: list of dictionaries
-                individual entires are in a dict within the collecitve list
+        runs all functions in order of the pipeline
         '''
         self.get_number_of_entries(db)
         self.get_entries(reduce_searches, db)
         self.classify_vh_vl()
+        self.find_antigen()
+        self.find_fragment_id()
         self.group_by_author_title()
-
-# add function to join vl and vh
-# add functions to search entries desctiptions if covid is mentioned
-# add functions to populate a csv
+        self.pair_vh_vl()
 
 
 if __name__ == '__main__':
@@ -124,4 +296,4 @@ if __name__ == '__main__':
                'OR bind[All Fields] OR inhibit[All Fields] ' +\
                'OR anti-Sars-Cov-2[All Fields]))'
     genbanksearch = GenbankSearch(keywords)
-    genbanksearch(reduce_searches=50)
+    genbanksearch(reduce_searches=100)
--- genbank/test_mine_genbank.py
+++ genbank/test_mine_genbank.py
@@ -11,39 +11,53 @@ class TestMineGenbank(unittest.TestCase):
         super().__init__(*argv, **kwarg)
         Entrez.email = "fabian.spoendlin@exeter.ox.ac.uk"
 
+        # initially search one entry
         self.search = 'QQX23546[Accession]'
         self.genbank = mine_genbank.GenbankSearch(self.search)
         self.genbank.get_number_of_entries()
         self.genbank.get_entries()
+        # example of entires to test the functions
         self.entries_list = [{'GBSeq_references': [
                                  {'GBReference_authors': 'Author 1',
                                   'GBReference_title': 'Title 1'}],
-                              'GBSeq_definition': 'heavy'},
+                              'GBSeq_definition': 'HEAVY, SARS-cov, S3-F23'},
                              {'GBSeq_references': [
                                  {'GBReference_authors': 'Author 1',
                                   'GBReference_title': 'Title 1'}],
-                              'GBSeq_definition': 'alpha'},
+                              'GBSeq_definition': 'alpha, covid-19, RBD, A12'},
                              {'GBSeq_references': [
                                  {'GBReference_authors': 'Author 2',
-                                  'GBReference_title': 'Title 2'}],
+                                  'GBReference_title': 'Title 2, merS-cOv'}],
                               'GBSeq_definition': 'kappa'},
                              {'GBSeq_references': [
                                  {'GBReference_authors': 'Author 2',
-                                  'GBReference_title': 'Title 2'}],
+                                  'GBReference_title': 'Title 2, merS-cOv'}],
                               'GBSeq_definition': 'omicron'},
                              {'GBSeq_references': [
                                  {'GBReference_authors': 'Author 2',
                                   'GBReference_title': 'Title 1'}],
-                              'GBSeq_definition': 'lambda'}]
+                              'GBSeq_definition': 'LAMBDA, ABD123'}]
+
+        # list of ids of genbank entries, 1-2 & 3-4 are vh-vl pairings
+        # 5-6 & 7-8 are from the same publication but not pairings
+        self.ids = ['7WDF_F', '7WDF_E', '7WD7_f', '7WD7_e', '7WD7_C', '7WCZ_B',
+                    'CAB4059600', 'CAB4065462']
+
+        entires_handle = Entrez.efetch(db='protein', id=self.ids,
+                                       rettype="gb", retmode="xml")
+        self.entries_gb = Entrez.read(entires_handle)
 
     def test_get_number_of_entries(self):
         self.assertEqual(self.genbank.number_of_entries, 1)
 
     def test_get_entries(self):
+        # check that the formatting of the dowloaded entry is correct
         self.assertIsInstance(self.genbank.entries, list)
         self.assertIsInstance(self.genbank.entries[0], dict)
+        # check that the correct (protein) database was searched
         self.assertEqual(self.genbank.entries[0]['GBSeq_moltype'], 'AA')
 
+        # check that the downloaded sequence is correct
         self.sequence = 'evqllesgggvvqpgrslrlscaasgftfssygmhwvrqapgkglewvav' +\
                         'iwydgsnkyyadsvkgrftisrdnskntlylqmnslraedtavyycardg' +\
                         'sgsyywgsldywgqgtlvtvss'
@@ -51,8 +65,10 @@ class TestMineGenbank(unittest.TestCase):
                          self.sequence)
 
     def test_classify_vh_vl(self):
+        # overwrite entires with example entries
         self.genbank.entries = self.entries_list
         self.genbank.classify_vh_vl()
+        # check if VHs and VLs in examples were classified correctly
         entries = self.genbank.entries
         self.assertEqual(entries[0]['chain'], 'heavy_chain')
         self.assertEqual(entries[1]['chain'], 'heavy_chain')
@@ -60,12 +76,40 @@ class TestMineGenbank(unittest.TestCase):
         self.assertEqual(entries[3]['chain'], 'unassigned')
         self.assertEqual(entries[4]['chain'], 'light_chain')
 
+    def test_find_antigen(self):
+        # overwrite entires with example entries
+        self.genbank.entries = self.entries_list
+        self.genbank.find_antigen()
+        # check if the antigen of the example entries was detected correctly
+        entries = self.genbank.entries
+        self.assertEqual(entries[0]['antigen'], 'SARS-CoV')
+        self.assertEqual(entries[1]['antigen'],
+                         'SARS-CoV-2, Spike protein RBD')
+        self.assertEqual(entries[2]['antigen'], 'MERS-CoV')
+        self.assertEqual(entries[3]['antigen'], 'MERS-CoV')
+        self.assertEqual(entries[4]['antigen'], 'not determined')
+
+    def test_find_fragment_id(self):
+        # overwrite entires with example entries
+        self.genbank.entries = self.entries_list
+        self.genbank.find_fragment_id()
+        # check if the fragment id of the example entries was determined
+        # correctly
+        entries = self.genbank.entries
+        self.assertEqual(entries[0]['fragment_id'], 'S3-F23')
+        self.assertEqual(entries[1]['fragment_id'], 'A12')
+        self.assertEqual(entries[4]['fragment_id'], 'ABD123')
+        with self.assertRaises(KeyError):
+            entries[2]['fragment_id']
+
     def test_group_by_author_title(self):
+        # overwrite entires with example entries
         self.genbank.entries = self.entries_list
         self.genbank.classify_vh_vl()
         self.genbank.group_by_author_title()
         grouped_entries = self.genbank.grouped_entries
 
+        # check if the example entires were correctly grouped
         self.assertEqual(len(grouped_entries), 3)
         self.assertEqual(len(grouped_entries[0]), 2)
 
@@ -79,9 +123,46 @@ class TestMineGenbank(unittest.TestCase):
         title_g2 = g2['GBReference_title']
         title_g3 = g3['GBReference_title']
 
+        # check that all groups differ in either title or authors
         self.assertFalse(title_g1 == title_g2 and author_g1 == author_g2)
         self.assertFalse(title_g1 == title_g3 and author_g1 == author_g3)
         self.assertFalse(title_g2 == title_g3 and author_g2 == author_g3)
+
+    def test_pair_vh_vl(self):
+        # overwrite entires with genbank entries
+        self.genbank.entries = self.entries_gb
+        self.genbank.classify_vh_vl()
+        self.genbank.find_antigen()
+        self.genbank.find_fragment_id()
+        self.genbank.group_by_author_title()
+        self.genbank.pair_vh_vl()
+
+        # check that the total number of entries after pairing equals
+        # the total number of entires dowloaded from genbank
+        total_entries = len(self.genbank.paired_entries) * 2
+        # multiplied by two as each pairing has one heavy and light chain
+        for group in self.genbank.unpaired_entries:
+            total_entries += len(group)
+        self.assertEqual(total_entries, len(self.ids))
+
+        # check that the number of entires in each category are correct
+        self.assertEqual(len(self.genbank.paired_entries), 2)
+        self.assertEqual(len(self.genbank.unpaired_entries), 2)
+        self.assertEqual(len(self.genbank.unpaired_entries[0]), 2)
+        self.assertEqual(len(self.genbank.unpaired_entries[1]), 2)
+
+        for entry in self.genbank.paired_entries:
+            # check that the chains are correct in the dictionary
+            self.assertEqual(entry['heavy_chain']['chain'], 'heavy_chain')
+            self.assertEqual(entry['light_chain']['chain'], 'light_chain')
+
+            # check that the pairings are correct
+            if entry['heavy_chain']['GBSeq_locus'] == '7WDF_F':
+                self.assertTrue(
+                    entry['light_chain']['GBSeq_locus'] == '7WDF_E')
+            if entry['heavy_chain']['GBSeq_locus'] == '7WD7_f':
+                self.assertTrue(
+                    entry['light_chain']['GBSeq_locus'] == '7WD7_e')
 
 
 if __name__ == '__main__':
