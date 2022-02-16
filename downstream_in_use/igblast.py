7d778f713b3a170a1cd1b533da2bbaf8fcc177c0
gemma gordon <gemmagordon2012@gmail.com>
2022-02-16 13:53:42 +0000

    working on igblast

---
+++ downstream_in_use/igblast.py
@@ -0,0 +1,8 @@
+
+# would have to load in VH_seq and VL_seq as strings first e.g. from dataframe
+
+def nt_to_aa(VH_seq, VL_seq):
+
+    '''convert nucleotide sequences from genbank or straight from papers to aa sequences'''
+
+    return aa_1_seq
