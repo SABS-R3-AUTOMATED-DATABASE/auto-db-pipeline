17707379616ded775c598eb66a8ecf6348d27ddb
gemma gordon <gemmagordon2012@gmail.com>
2022-03-01 12:22:54 +0000

    IGblast working

--- downstream_in_use/igblastnprocess.py
+++ downstream_in_use/igblastnprocess.py
@@ -2,39 +2,29 @@ import os, time
 import subprocess
 import pandas as pd
 
-# NOTE need local database to run
-# NOTE create fasta from sequences for input 'fasta_name'
 
-class IGBLASTprocess:
+def create_nseq_dict(VH_nseq, VL_nseq):  # NOTE basically identical to create_seq_dict function in verify_antibody script
 
-    def __init__(self, ncpu=15):
+    '''create seq dictionary for input to igblast if nucleotide sequences extracted from genbank or direct from papers
 
-        self.ncpu = ncpu
-        self.germlines = '../data/germlines'  # NOTE change file path
-        self.tmpfile = ''  # NOTE add in path to temp file for igblast output
+        dict has keys VH and VL and values are corresponding nucleotide seqs'''
 
-    def __call__(self, fasta_name, organism):
+    nseq_dict = {"VH_nseq": VH_nseq, "VL_nseq": VL_nseq}
 
-        start_time = time.time()
-        print("Running Igblastn:", fasta_name)
+    print(nseq_dict)
 
-        vdb, ddb, jdb = get_vdj_of_species(organism, self.germlines)
+    return nseq_dict
 
-        subprocess.run(["igblastn", "-germline_db_V", vdb, "-germline_db_D", ddb, "-germline_db_J", jdb, "-organism", organism,
-                        "-show_translation", "-num_alignments_D", "1", "-num_alignments_V", "1", "-ig_seqtype", "Ig", 
-                        "-num_clonotype", "0", "-num_alignments_J", "1", "-outfmt", "19", "-auxiliary_data", "optional_file/{}_gl.aux".format(organism),
-                        "-query", fasta_name, "-out", self.tmpfile, "-num_threads", "{}".format(self.ncpu)], check = True)       
 
-        # RE-SAVE THE OUTPUT FILE AS A CSV
-        # NOTE will be output for a single sequence to CSV
-        igblast_df = pd.read_csv(self.tmpfile, sep="\t")
+def create_igblast_input(seq_dict):
 
-        os.remove(self.tmpfile)  # remove temp IgBLAST output once created CSV
-        os.remove(fasta_name)  # remove fasta once processed sequences with IgBLAST
+    '''Creates FASTA file as input format for IgBLAST'''
 
-        print("Igblastn finished. Took:", time.time()-start_time)
+    with open('igblast_input.fasta', 'a+') as igblast_input:
+        for key, value in seq_dict.items():
+            igblast_input.write('>%s\n%s\n' % (key, value))
 
-        return igblast_df
+    return igblast_input
 
 
 def get_vdj_of_species(organism, germline_path):
@@ -44,3 +34,52 @@ def get_vdj_of_species(organism, germline_path):
     jdb = "{}/{}_database/{}_VJ_DNA".format(germline_path, organism, organism)
 
     return vdb, ddb, jdb
+
+class IGBLASTprocess:
+
+    def __init__(self, ncpu=15):
+
+        self.ncpu = ncpu
+        self.germlines = 'auto-db-pipeline/downstream_in_use/germlines'  # NOTE file path will need changing for final version 
+        self.tmpfile = 'auto-db-pipeline/igblast_output.csv'  # create temp output file, will be deleted 
+
+    def __call__(self, fasta_name, organism):
+
+        start_time = time.time()
+        print("Running Igblastn:", fasta_name)
+
+        vdb, ddb, jdb = get_vdj_of_species(organism, self.germlines)
+
+        subprocess.run(["igblastn", "-germline_db_V", vdb, "-germline_db_D", ddb, "-germline_db_J", jdb, "-organism", organism,
+                        "-show_translation", "-num_alignments_D", "1", "-num_alignments_V", "1", "-ig_seqtype", "Ig",
+                        "-num_clonotype", "0", "-num_alignments_J", "1", "-outfmt", "19", "-auxiliary_data", "optional_file/{}_gl.aux".format(organism),
+                        "-query", fasta_name, "-out", self.tmpfile, "-num_threads", "{}".format(self.ncpu)], check=True)   
+
+        # csv output read into dataframe with only relevant columns
+        igblast_df = pd.read_csv(self.tmpfile, sep="\t", usecols=['locus', 'productive', 'v_call', 'j_call', 'sequence_alignment_aa', 'cdr3_aa'])
+
+        os.remove(self.tmpfile)  # remove temp IgBLAST output once created CSV
+        #os.remove(fasta_name)  # remove fasta once processed sequences with IgBLAST
+
+        print("Igblastn finished. Took:", time.time()-start_time)
+
+        print(igblast_df)
+
+        return igblast_df
+
+
+
+
+# run test sequences
+
+VH_nseq='GAAGTGCAGCTGGTGGAGTCTAGAGCAGAGATGAAAAAGCCCGGGGAGTCTCTGAAGATATCCTGTAAGGGTTCTGGATACACCTTTACCAACCACTGGATCGCCTGGGTGCGCCAGATGCCCGGGAAAGGCCTGGAGTGGATGGGGATCATCTATCCTGGTGACTCTGACACCAGATACAGCCCGTCCTTCGAAGGCCAGGTCACCATCTCAGCCGACAAGTCCATCAGCACCGCCTACCTGCAGTGGAGCAGCCTGAAGGCCTCGGACACCGCCATGTATTACTGTGCGAGACGTGGATATACCTATGGTGCCGACTTTTACGGTTTGGACGTCTGGGGCCAAGGGACCAGGGTCACCGTCTCTTCA'
+VL_nseq='GATATTGTGATGACCCAGTCTCCAGACTCCCTGGCTGTGTCTCTGGGCGAGAGGGCCACCATCAACTGCAAGTCCAGCCAGAGTGTTTTATACAGCTCCAACAATAAGAACTACTTAGCTTGGTACCAGCAGAAACCAGGACAGCCTCCTAAGCTGCTCATTTACTGGGCATCTACCCGGGAATCCGGGGTCCCTGACCGATTCAGTGGCAGCGGGTCTGGGACAGATTTCACTCTCACCATCAGCAGCCTGCAGGCTGAAGATGTGGCAGTTTATTACTGTCAGCAATATTATAGTACTCCAATCACCTTCGGCCAAGGGACACGACTGGAGATTAAAC'
+
+nseq_dict = create_nseq_dict(VH_nseq=VH_nseq.upper(), VL_nseq=VL_nseq.upper())
+igblast_input = create_igblast_input(nseq_dict)
+
+
+igblastprocess = IGBLASTprocess()
+igblastprocess(fasta_name='igblast_input.fasta', organism='human')
+get_vdj_of_species(organism='human', germline_path='germlines')
+
