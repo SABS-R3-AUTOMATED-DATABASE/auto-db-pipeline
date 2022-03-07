bcc4727effa7afc4b8cb7e014b9edc8c0471c3d4
rsqgtms <qhy960729@gmail.com>
2022-03-07 16:16:38 +0000

    enabling it to search for nanobodies

--- patents/patent_scrape.py
+++ patents/patent_scrape.py
@@ -14,7 +14,7 @@ from datetime import datetime
 class Patents:
     def __init__(self) -> None:
         self.search_results = None
-        self.filtered_results = None
+        self.output = None
 
     def get_random_ua():
         """Function that gets a random user agent to access the webpages"""
@@ -59,7 +59,7 @@ class Patents:
                 patent_number.append(str(num))
         return results
 
-    def get_patents(self, CN = False):
+    def get_patents(self, CN=False):
         """This function taks around 6 hours to run to prevent getting blocked for accessing too many times in a short period of time"""
         patents = Patents.get_patent_urls()
         if CN is True:
@@ -130,62 +130,326 @@ class Patents:
             content = soup.find("div", class_="abstract")
             if content is not None:
                 df.loc[i, "Abstract"] = content.text
-            if (i+1) % 10 == 0:
-                print(str(i+1)+'/'+str(df.shape[0]),datetime.now())
+            if (i + 1) % 10 == 0:
+                print(str(i + 1) + "/" + str(df.shape[0]), datetime.now())
             if i % 100 == 0 and i != 0:
                 time.sleep(1800)
         self.search_results = df
         return df
 
-    def save_search_output(self, filepath: str):
-        self.search_results.to_json(filepath)
+    def extract_seq_from_id(content: list, id):
+        splited_text = "".join(content).split("<210>")
+        seq = ""
+        origin = ""
+        if len(splited_text) > 1:
+            seqs = []
+            origins = []
+            for i in range(1, len(splited_text)):
+                if len(splited_text[i].split(">")) > 1:
+                    seqs.append(splited_text[i].split(">")[-1])
+                    origins.append(splited_text[i].split(">")[-2][:-4])
+            for elem in seqs:
+                item = re.findall("\A\s*\d+(?!\d)", elem)
+                if len(item) > 0:
+                    item = item[0].replace(" ", "")
+                    if item == id:
+                        seq = re.sub("\d+", "", elem)
+                        seq = re.sub("\s+(?!\s)", " ", seq)
+                        origin = origins[seqs.index(elem)]
+        return seq, origin
 
-    def load_search_output(self, filepath: str):
-        self.search_results = pd.read_json(filepath)
+    def CN113817052A(Content: list, URL: str):
+        outputdf = pd.DataFrame(
+            {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+            dtype="str",
+        )
+        items = [
+            ["1", "2"],
+            ["3", "4"],
+            ["5", "6"],
+            ["7", "8"],
+            ["9", "10"],
+            ["11", "12"],
+            ["13", "14"],
+        ]
+        for item in items:
+            hcseq, hco = Patents.extract_seq_from_id(Content, item[0])
+            lcseq, lco = Patents.extract_seq_from_id(Content, item[1])
+            dummydf = pd.DataFrame(
+                {
+                    "URL": [URL],
+                    "HCVR": [hcseq],
+                    "LCVR": [lcseq],
+                    "HCOrigin": [hco],
+                    "LCOrigin": [lco],
+                },
+                dtype="str",
+            )
+            outputdf = pd.concat([outputdf, dummydf], axis=0)
+        return outputdf
 
-    def process_results(self):
-        df = self.search_results
+    def CN111978395A(Content: list, URL: str):
+        outputdf = pd.DataFrame(
+            {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+            dtype="str",
+        )
+        items = [
+            ["1", "18"],
+            ["2", "19"],
+            ["3", "20"],
+            ["4", "21"],
+            ["5", "22"],
+            ["6", "23"],
+            ["7", "24"],
+            ["8", "25"],
+            ["9", "26"],
+            ["10", "27"],
+            ["11", "28"],
+            ["12", "29"],
+            ["13", "30"],
+            ["14", "31"],
+            ["15", "32"],
+            ["16", "33"],
+            ["17", "34"],
+        ]
+        for item in items:
+            hcseq, hco = Patents.extract_seq_from_id(Content, item[0])
+            lcseq, lco = Patents.extract_seq_from_id(Content, item[1])
+            dummydf = pd.DataFrame(
+                {
+                    "URL": [URL],
+                    "HCVR": [hcseq],
+                    "LCVR": [lcseq],
+                    "HCOrigin": [hco],
+                    "LCOrigin": [lco],
+                },
+                dtype="str",
+            )
+            outputdf = pd.concat([outputdf, dummydf], axis=0)
+        return outputdf
+
+    def extract_seq_from_string(elem: str, Content: list, URL: str):
+        if "cdr" not in elem and "fc" not in elem:
+            if "light and heavy chain" in elem:
+                outputdf = pd.DataFrame(
+                    {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+                    dtype="str",
+                )
+                items = re.findall(
+                    "(?<=seq id no:)\d+\s*and\s*[seq id no:]*\s*\d+", elem
+                )
+                if len(items) != 0:
+                    for item in items:
+                        item = re.findall("\d+", item)
+                        hcseq, hco = Patents.extract_seq_from_id(Content, item[1])
+                        lcseq, lco = Patents.extract_seq_from_id(Content, item[0])
+                        outputdf = pd.concat(
+                            [
+                                outputdf,
+                                pd.DataFrame(
+                                    {
+                                        "URL": [URL],
+                                        "HCVR": [hcseq],
+                                        "LCVR": [lcseq],
+                                        "HCOrigin": [hco],
+                                        "LCOrigin": [lco],
+                                    },
+                                    dtype="str",
+                                ),
+                            ],
+                            axis=0,
+                        )
+                return outputdf
+            if "heavy and light chain" in elem:
+                outputdf = pd.DataFrame(
+                    {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+                    dtype="str",
+                )
+                items = re.findall(
+                    "(?<=seq id no:)\d+\s*and\s*[seq id no:]*\s*\d+", elem
+                )
+                if len(items) != 0:
+                    for item in items:
+                        item = re.findall("\d+", item)
+                        hcseq, hco = Patents.extract_seq_from_id(Content, item[0])
+                        lcseq, lco = Patents.extract_seq_from_id(Content, item[1])
+                        outputdf = pd.concat(
+                            [
+                                outputdf,
+                                pd.DataFrame(
+                                    {
+                                        "URL": [URL],
+                                        "HCVR": [hcseq],
+                                        "LCVR": [lcseq],
+                                        "HCOrigin": [hco],
+                                        "LCOrigin": [lco],
+                                    },
+                                    dtype="str",
+                                ),
+                            ],
+                            axis=0,
+                        )
+                return outputdf
+            if (
+                ("heavy chain" in elem and "light chain" in elem)
+                or ("hcvr" in elem and "lcvr" in elem)
+                or ("vh" in elem and "vl" in elem)
+            ):
+                item = re.findall("(?<=seq id no:)\d+", elem)
+                if len(item) == 2 and item[0] != item[1]:
+                    hcindex = max(
+                        elem.find("heavy chain"), elem.find("hcvr"), elem.find("vh")
+                    )
+                    lcindex = max(
+                        elem.find("light chain"), elem.find("lcvr"), elem.find("vl")
+                    )
+                    if hcindex != -1 and lcindex != -1:
+                        if hcindex > lcindex:
+                            hcseq, hco = Patents.extract_seq_from_id(Content, item[1])
+                            lcseq, lco = Patents.extract_seq_from_id(Content, item[0])
+                        else:
+                            hcseq, hco = Patents.extract_seq_from_id(Content, item[0])
+                            lcseq, lco = Patents.extract_seq_from_id(Content, item[1])
+                        return pd.DataFrame(
+                            {
+                                "URL": [URL],
+                                "HCVR": [hcseq],
+                                "LCVR": [lcseq],
+                                "HCOrigin": [hco],
+                                "LCOrigin": [lco],
+                            },
+                            dtype="str",
+                        )
+            elif "nanobody" in elem or "single domain antibody" in elem:
+                outputdf = pd.DataFrame(
+                    {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+                    dtype="str",
+                )
+                item = re.findall("(?<=seq id no:)\d+", elem)
+                item2 = re.findall("(?<=seq id no:)\d+(?!\d)\s*\-*\s*\d+", elem)
+                if len(item2) != 0:
+                    item2 = re.findall("\d+", item2[0])
+                    item2 = [
+                        str(i) for i in list(range(int(item2[0]), 1 + int(item2[1])))
+                    ]
+                items = list(set(item) | set(item2))
+                if len(items) != 0:
+                    for item in items:
+                        hcseq, hco = Patents.extract_seq_from_id(Content, item)
+                        outputdf = pd.concat(
+                            [
+                                outputdf,
+                                pd.DataFrame(
+                                    {
+                                        "URL": [URL],
+                                        "HCVR": [hcseq],
+                                        "LCVR": [""],
+                                        "HCOrigin": [hco],
+                                        "LCOrigin": [""],
+                                    },
+                                    dtype="str",
+                                ),
+                            ],
+                            axis=0,
+                        )
+                outputdf = outputdf.reset_index(drop=True)
+                return outputdf
+
+    def extract_VH_VL(self, df=None):
+        if df == None:
+            df = self.search_results
         df = df[
             df.Title.str.contains("sars-cov-2")
             | df.Title.str.contains("covid")
             | df.Title.str.contains("coronavirus")
         ]
         df = df.reset_index(drop=True)
-        df["Note"] = ""
-        df["Note"] = df["Note"].astype(object)
-        df["Seq"] = ""
-        df["Seq"] = df["Seq"].astype(object)
+        outputdf = pd.DataFrame(
+            {"URL": [], "HCVR": [], "LCVR": [], "HCOrigin": [], "LCOrigin": []},
+            dtype="str",
+        )
         for i in range(df.shape[0]):
-            notes = []
-            seqid = []
-            for _ in df.loc[i, "Claim"]:
-                if (
-                    "heavy chain"
-                    or "cdr"
-                    or "determining region"
-                    or "hcvr"
-                    or "light chain"
-                    or "lcvr" in _.lower()
-                ):
-                    if "seq id no" or "amino acid sequence" in _.lower():
-                        notes.append(_)
-                        item = re.findall("seq id no:*\.*\s*\d+", _.lower())
-                        seqid = list(set(seqid) | set(item))
-            df.at[i, "Note"] = notes
-            df.at[i, "Seq"] = seqid
-        df = df[df["Note"] != pd.Series([[]] * df.shape[0])]
-        df = df.reset_index(drop=True)
-        self.filtered_results = df
+            if df.loc[i, "URL"] == "https://patents.google.com/patent/CN113817052A/en":
+                seqdf = Patents.CN113817052A(
+                    Content=df.loc[i, "Content"], URL=df.loc[i, "URL"]
+                )
+                outputdf = pd.concat([outputdf, seqdf], axis=0)
+            elif (
+                df.loc[i, "URL"] == "https://patents.google.com/patent/CN111978395A/en"
+            ):
+                seqdf = Patents.CN111978395A(
+                    Content=df.loc[i, "Content"], URL=df.loc[i, "URL"]
+                )
+                outputdf = pd.concat([outputdf, seqdf], axis=0)
+            else:
+                for _ in df.loc[i, "Claim"]:
+                    if "seq id no" in _.lower():
+                        edited = re.sub(
+                            "seq id nos*:*\.*\s*(?=\d+)", "seq id no:", _.lower()
+                        )
+                        edited = re.split("\. | \d+\)", edited)
+                        for elem in edited:
+                            seqdf = Patents.extract_seq_from_string(
+                                elem=elem,
+                                Content=df.loc[i, "Content"],
+                                URL=df.loc[i, "URL"],
+                            )
+                            if seqdf is not None:
+                                outputdf = pd.concat([outputdf, seqdf], axis=0)
+        outputdf.drop_duplicates(keep="first", inplace=True)
+        outputdf = outputdf.reset_index(drop=True)
+        outputdf = outputdf[outputdf["HCVR"] != pd.Series([""] * outputdf.shape[0])]
+        outputdf = outputdf.reset_index(drop=True)
+        self.output = outputdf
+        return outputdf
 
-    def save_final_results(self, filepath: str):
-        self.filtered_results.to_json(filepath)
+    def save_search_output(self, filepath: str = "patents/search_results.json"):
+        self.search_results.to_json(filepath)
 
-    def load_read_final_output(self, filepath: str):
-        self.filtered_results = pd.read_json(filepath)
+    def load_search_output(self, filepath: str = "patents/search_results.json"):
+        self.search_results = pd.read_json(filepath)
+
+    def save_final_output(self, filepath: str = "patents/final_results.csv"):
+        self.output.to_csv(filepath)
+
+    # def process_results(self):
+    #     df = self.search_results
+    #     df = df[
+    #         df.Title.str.contains("sars-cov-2")
+    #         | df.Title.str.contains("covid")
+    #         | df.Title.str.contains("coronavirus")
+    #     ]
+    #     df = df.reset_index(drop=True)
+    #     df["Note"] = ""
+    #     df["Note"] = df["Note"].astype(object)
+    #     df["Seq"] = ""
+    #     df["Seq"] = df["Seq"].astype(object)
+    #     for i in range(df.shape[0]):
+    #         notes = []
+    #         seqid = []
+    #         for _ in df.loc[i, "Claim"]:
+    #             if (
+    #                 "heavy chain"
+    #                 or "cdr"
+    #                 or "determining region"
+    #                 or "hcvr"
+    #                 or "light chain"
+    #                 or "lcvr" in _.lower()
+    #             ):
+    #                 if "seq id no" or "amino acid sequence" in _.lower():
+    #                     notes.append(_)
+    #                     item = re.findall("seq id nos*S:*\.*\s*\d+", _.lower())
+    #                     seqid = list(set(seqid) | set(item))
+    #         df.at[i, "Note"] = notes
+    #         df.at[i, "Seq"] = seqid
+    #     df = df[df["Seq"] != pd.Series([[]] * df.shape[0])]
+    #     df = df.reset_index(drop=True)
+    #     self.filtered_results = df
 
 
 test = Patents()
-test.get_patents(CN = True)
-test.save_search_output("patents/search_results.json")
-test.process_results()
-test.save_final_results("patents/final_results.json")
+# test.get_patents(CN = True)
+# test.save_search_output("patents/search_results.json")
+test.load_search_output()
+test.extract_VH_VL()
+test.save_final_results()
