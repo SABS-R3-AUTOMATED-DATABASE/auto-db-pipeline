622553fd0766e2db9b0cd687715eb2983aed1038
gemma gordon <gemmagordon2012@gmail.com>
2022-03-14 21:44:39 +0000

    fixed bugs

--- get_additional_info/seq_correction_add_info.py
+++ get_additional_info/seq_correction_add_info.py
@@ -3,6 +3,7 @@ import pandas as pd
 from igblastnprocess import IGBLASTprocess
 from igblastnprocess import get_vdj_of_species
 from retrieve_cdr_germlines import get_CDRs_and_germlines
+import re
 
 igblastprocess = IGBLASTprocess()
 
@@ -16,10 +17,10 @@ def standardise_seqs(csv_file, vh_col_name, vl_col_name):
 
     # convert patent output to dataframe
     df = pd.read_csv(csv_file)
+    print(df.columns)
 
     # go through rows of sequences columns in df and standardise to single letter AA format
     aa_3_characters = ['Cys', 'Asp', 'Ser', 'Gln', 'Lys', 'Ile', 'Pro', 'Thr', 'Phe', 'Asn', 'Gly', 'His', 'Leu', 'Arg', 'Trp', 'Ala', 'Val', 'Glu', 'Tyr', 'Met']
-    nt_characters = ['A', 'C', 'T', 'G']
 
     formatted_vh = []
     formatted_vl = []
@@ -27,13 +28,13 @@ def standardise_seqs(csv_file, vh_col_name, vl_col_name):
     # for each sequence in each of the columns of sequences, check format and change if necessary (e.g. from 3 letter of NT seqs to single letter AA)
     for col in [df[vh_col_name], df[vl_col_name]]:
         for row in col:
-            # if in 3 letter format change to single letter AA 
-            if any(aa_3_character in row for aa_3_character in aa_3_characters):
+            # if in 3 letter format change to single letter AA
+            if any(aa_3_character in str(row) for aa_3_character in aa_3_characters):
                 # strip whitespace and convert to single character AA seq
                 row = seq1(row.replace(' ', ''))
             # if a nucleotide sequence use IgBLAST to convert to AA
-            elif any(nt_character in row.upper() for nt_character in nt_characters):
-                row = row.replace(' ', '').upper()
+            elif bool(re.match('^ACTG]+$', str(row).upper())) is True:
+                row = str(row).replace(' ', '').upper()
                 # run igblast to convert to amino acid sequence
                 with open('igblast_input.fasta', 'a+') as igblast_input:
                     igblast_input.write('>seq\n%s\n' % row)
@@ -51,11 +52,12 @@ def standardise_seqs(csv_file, vh_col_name, vl_col_name):
                 formatted_vl.append(row)
 
     # drop old seqs columns and add in new lists as cols
-    df['VH'] = formatted_vh
-    df['VL'] = formatted_vl
     df.drop(vh_col_name, axis=1, inplace=True)
     df.drop(vl_col_name, axis=1, inplace=True)
+    df['VH'] = formatted_vh
+    df['VL'] = formatted_vl
     df = df.fillna(value='N/A')
+    print(df.columns)
 
     return df
 
@@ -63,7 +65,7 @@ def standardise_seqs(csv_file, vh_col_name, vl_col_name):
 def correction_and_add_cdrs(df, outfile_name):
 
     '''filter sequences to check anarci, if ok then get cdrs and germlines; add this extra info to a new final df
-    count how many sequences lost by failing anarci'''
+    count how many sequences lost by N/A (e.g. if unpaired) or failing anarci'''
 
     # go through new cols and correct sequence length with anarci
     corrected_vh = []
@@ -72,6 +74,7 @@ def correction_and_add_cdrs(df, outfile_name):
     vl_cdrs = []
 
     seqs_lost = 0
+    seqs_na = 0
 
     # loop through newly created cols of formatted sequences
     for col in [df['VH'], df['VL']]:
@@ -79,7 +82,7 @@ def correction_and_add_cdrs(df, outfile_name):
             if col is df['VH']:
                 # if blank row, add empty dict
                 if row == 'N/A':
-                    seqs_lost =+ 1
+                    seqs_na += 1
                     seq = dict()
                     cdrs = dict()
                     corrected_vh.append(seq)
@@ -106,7 +109,7 @@ def correction_and_add_cdrs(df, outfile_name):
             elif col is df['VL']:
                 # if blank row, add empty dict
                 if row == 'N/A':
-                    seqs_lost += 1
+                    seqs_na += 1
                     seq = dict()
                     cdrs = dict()
                     corrected_vl.append(seq)
@@ -129,19 +132,20 @@ def correction_and_add_cdrs(df, outfile_name):
                         corrected_vl.append(seq)
                         vl_cdrs.append(cdrs)
 
+    seq_total = len(df['VH']) + len(df['VL'])
+    print('Total number of sequences input: ', seq_total)
+    print('Total sequences N/A from input: ', seqs_na)
     print('Total sequences lost after failing ANARCI filtering: ', seqs_lost)
-
-    # change lists to df and rearrange col order
-    corrected_vh = pd.DataFrame(corrected_vh)
-    corrected_vh = corrected_vh[['VH', 'VL']]
-    corrected_vl = pd.DataFrame(corrected_vl)
-    corrected_vl = corrected_vl[['VH', 'VL']]
-    vh_cdrs = pd.DataFrame(vh_cdrs)
-    vl_cdrs = pd.DataFrame(vl_cdrs)
 
     # remove old VH and VL rows from df
     df.drop('VH', axis=1, inplace=True)
     df.drop('VL', axis=1, inplace=True)
+
+    # change lists to df and rearrange col order
+    corrected_vh = pd.DataFrame(corrected_vh)
+    corrected_vl = pd.DataFrame(corrected_vl)
+    vh_cdrs = pd.DataFrame(vh_cdrs)
+    vl_cdrs = pd.DataFrame(vl_cdrs)
 
     # add in new corrected VH and VL rows
     seqs_updated = corrected_vh.combine_first(corrected_vl)
@@ -155,5 +159,5 @@ def correction_and_add_cdrs(df, outfile_name):
     return corrected_seqs_with_cdrs
 
 
-df = standardise_seqs(csv_file='patentoutput.csv', vh_col_name='HCVR', vl_col_name='LCVR')
-final_df = correction_and_add_cdrs(df=df, outfile_name='test_generalised_code.csv')
+df = standardise_seqs(csv_file='results3letters.csv', vh_col_name='HCVR', vl_col_name='LCVR')
+final_df = correction_and_add_cdrs(df=df, outfile_name='genbank_output.csv')
