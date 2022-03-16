c6efee548d87f8d5b1bbda12f048907dc50bfabb
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2022-03-16 12:54:43 +0000

    run genbank pipeline added to main

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+import imp
 import pandas as pd
 import numpy as np
 import json
 
-from genbank.keywords2ids import GenbankSearch
-from genbank.ids2protein import ProteinRetrieval
-from genbank.proteins2info import InfoRetrieval
-from genbank.info2csv import PopulateDatabase
-from genbank.evaluate_genbank_search import EvaluateGenbankSearch
-
+from auto_db_pipeline.genbank.run_genbank_pipeline import run_genbank_pipeline
 from auto_db_pipeline.papers2ids import Papers
 
 # TODO: multiprocessing after keywords are generated
@@ -15,19 +11,6 @@ def main():
   ''' Get keywords for papers/genbank/patent search '''
   # TODO: write this function and make dynamics for functions below
   keywords = get_keywords() 
-
-  ''' Search for seqs from genbank IDs'''
-  genbanksearch = GenbankSearch(keywords)
-  genbanksearch()
-
-  proteinretrival = ProteinRetrieval()
-  proteinretrival()
-
-  inforetreival = InfoRetrieval()
-  inforetreival(classification_method='anarci')
-
-  populatedb = PopulateDatabase()
-  populatedb()
 
   ''' Search for seqs from patents'''
   patent_seqs = get_seqs_from_patents(keywords)
@@ -42,6 +25,10 @@ def main():
   paper_urls = get_dois()
   supp_seqs = get_seqs_from_supp(paper_urls)
 
+  ''' Search for seqs from genbank IDs'''
+  # needs to be run after jesses code
+  run_genbank_pipeline(keywords, path_to_jesses_ids, output_path)
+  
   ''' Combine all outputs and get statistics'''
     
 
