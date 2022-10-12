b904c382017e8d53c7114d47b3bdd2c9bdf9292e
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2022-10-12 12:26:28 +0100

    deleted unnecessary files and changed directories

--- auto_db_pipeline/run_new_pdbs.py
+++
@@ -1,5 +0,0 @@
-import json
-path_new = "../data/pdbs2info/new_pdb_ids.json"
-with open(path_new, "r", encoding="utf8") as f:
-    new2 = json.load(f)
-
--- auto_db_pipeline/run_pdbids2info.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('export NCBI_API_KEY="447726e72ee80490c9d97644aa34d3e27508"')  # set metapub key
-from pdbs2info import PdbIDs
-pdbs = PdbIDs(selected_date="2022_03_08")
-pdbs()
